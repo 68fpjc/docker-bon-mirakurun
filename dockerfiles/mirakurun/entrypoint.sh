122dec374fb20cc5440b56b759eecfd982e0ae62
68fpjc <32516363+68fpjc@users.noreply.github.com>
2020-07-31 22:04:32 +0900

    pcscdイメージを削除してもろもろ変更

--- dockerfiles/mirakurun/entrypoint.sh
+++ dockerfiles/mirakurun/entrypoint.sh
@@ -7,10 +7,16 @@ for BONCONF in config.in/*.so.conf ; do
   BONCLIENT=${BONCLIENT%.*}
   cp /usr/local/lib/BonDriver_Proxy.so ${BONCLIENT}
 done
-cd ..
 
-cd /usr/local/lib/node_modules/mirakurun/
-for CONF in channels tuners server ; do
-  [ ! -s /usr/local/etc/mirakurun/${CONF}.yml ] && cat config/${CONF}.yml > /usr/local/etc/mirakurun/${CONF}.yml
-done
-exec pm2 --no-daemon start processes.json
+[ -e /opt/bin/ ] || mkdir -p /opt/bin/
+if [ ! -e /opt/bin/exec-socat ] ;then
+  cat << 'EOF' > /opt/bin/exec-socat
+#! /bin/sh
+
+exec socat - tcp-connect:<REMOTE-HOST>:<REMOTE-PORT>
+EOF
+  chmod +x /opt/bin/exec-socat
+fi
+
+cd /app/
+exec docker/container-init.sh
