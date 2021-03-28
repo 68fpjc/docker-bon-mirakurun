feac0e0e7ac5b398170cff70964332d4da6d8b95
68fpjc <32516363+68fpjc@users.noreply.github.com>
2021-03-29 08:01:59 +0900

    mirakc 0.17.1

---
+++ buildx.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+pushd "$(dirname $0)"
+
+docker buildx create --name mybuilder
+
+# BonDriverProxy_Linux
+docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
+--tag 68fpjc/bondriverproxy:2021-03-02-armhf \
+dockerfiles/bon/
+
+# Mirakurun
+docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
+--tag 68fpjc/mirakurun:3.5.0-armhf \
+--build-arg BASE=chinachu/mirakurun:3.5.0 \
+dockerfiles/mirakurun/
+
+# mirakc
+docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
+--tag 68fpjc/mirakc:0.17.1-armhf \
+--build-arg BASE=mirakc/mirakc:0.17.1-debian \
+dockerfiles/mirakc/
+
+popd
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+for IMAGE in \
+68fpjc/bondriverproxy:2021-03-02 \
+68fpjc/mirakurun:3.5.0 \
+68fpjc/mirakc:0.17.1 \
+; do
+  ssh tvtuner docker image inspect ${IMAGE} &> /dev/null
+  if [ $? -ne 0 ]; then
+    echo Deploying ${IMAGE} ...
+    docker save ${IMAGE}-armhf | ssh tvtuner docker load \
+    && ssh tvtuner docker image tag ${IMAGE}-armhf ${IMAGE} \
+    && ssh tvtuner docker image rm ${IMAGE}-armhf
+  fi
+done
