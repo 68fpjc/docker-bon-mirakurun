726468370f7977c4ba36439e50f9b1509222612b
68fpjc <32516363+68fpjc@users.noreply.github.com>
2021-08-11 19:25:59 +0900

    Mirakurun 3.8.0

--- buildx.sh
+++ buildx.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 
-pushd "$(dirname $0)"
+cd "$(dirname $0)"
 
 docker buildx create --name mybuilder
 
 # BonDriverProxy_Linux
 docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
 --tag 68fpjc/bondriverproxy:2021-03-02-armhf \
-dockerfiles/bon/
+dockerfiles/bon/ \
+|| exit 1
 
 # Mirakurun
 docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
---tag 68fpjc/mirakurun:3.7.1-armhf \
---build-arg BASE=chinachu/mirakurun:3.7.1 \
-dockerfiles/mirakurun/
+--tag 68fpjc/mirakurun:3.8.0-armhf \
+--build-arg BASE=chinachu/mirakurun:3.8.0 \
+dockerfiles/mirakurun/ \
+|| exit 1
 
 # mirakc
 docker buildx build --builder mybuilder --platform linux/arm/v7 --load \
 --tag 68fpjc/mirakc:1.0.0-armhf \
 --build-arg BASE=mirakc/mirakc:1.0.0-debian \
-dockerfiles/mirakc/
-
-popd
+dockerfiles/mirakc/ \
+|| exit 1
